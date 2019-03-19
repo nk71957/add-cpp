ea479cbf4a410eb795a02fa935106df5905d12a9
nk71957 <nk71957@gmail.com>
2019-03-19 18:59:19 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout <<"enter the 1st number";
+    cin >> a;
+    cout <<"enter the 2nd number";
+    cin >>b;
+    cout <<"a+b=",a,b;
+    return 0;
+}
